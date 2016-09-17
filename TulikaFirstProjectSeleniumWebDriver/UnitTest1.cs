cfaa5870c26280acd3074ba637245259744eda76
PErsonal Computer <PErsonal Computer@TULIKA>
2016-09-17 18:31:51 +1000

    Add project files.

---
+++ TulikaFirstProjectSeleniumWebDriver/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+namespace TulikaFirstProjectSeleniumWebDriver
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver strdriver = new ChromeDriver();
+            strdriver.Navigate().GoToUrl("https://www.facebook.com/");
+            strdriver.Close();
+            strdriver.Quit();
+        }
+    }
+}
